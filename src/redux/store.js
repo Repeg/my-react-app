3fded54c82133f740151ac7865d7e991f277c80e
lipei5 <lipei5@lenovo.com>
2021-01-13 19:54:01 +0800

    redux 插件

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { counter } from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
     counter, 
-    applyMiddleware(thunk)//应用上异步中间件
+    composeWithDevTools(applyMiddleware(thunk))//应用上异步中间件
 );
 
 export default store;
